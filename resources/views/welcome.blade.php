9622cbef48bc66c66d1b274b370456006532dc17
Kulbir Singh <kulbir049@gmail.com>
2025-06-27 19:16:38 +0530

    pusehr

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,20 +2,19 @@
 <html>
 <head>
   <title>Pusher Test</title>
-  <script src="https://js.pusher.com/8.4.0/pusher.min.js"></script>
+ <script src="https://js.pusher.com/8.2.0/pusher.min.js"></script>
   <script>
+
+    // Enable pusher logging - don't include this in production
     Pusher.logToConsole = true;
 
-    const pusher = new Pusher('06e2099b131fedf4043a', {
-      cluster: 'ap2',
-      forceTLS: true
+    var pusher = new Pusher('06e2099b131fedf4043a', {
+      cluster: 'ap2'
     });
 
-    const channel = pusher.subscribe('my-channel');
-
+    var channel = pusher.subscribe('my-channel');
     channel.bind('my-event', function(data) {
-      console.log('hello');
-      alert('Received: ' + JSON.stringify(data));
+      alert(JSON.stringify(data));
     });
   </script>
 </head>
