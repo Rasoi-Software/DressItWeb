0b2c2fcc119518d85ef091def9f26c60b951e277
Kulbir Singh <kulbir049@gmail.com>
2025-06-27 18:41:05 +0530

    pusher

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,1 +1,25 @@
-<h1>Welcome DressIT</h1>
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Pusher Test</title>
+  <script src="https://js.pusher.com/8.4.0/pusher.min.js"></script>
+  <script>
+    Pusher.logToConsole = true;
+
+    const pusher = new Pusher('06e2099b131fedf4043a', {
+      cluster: 'ap2',
+      forceTLS: true
+    });
+
+    const channel = pusher.subscribe('my-channel');
+
+    channel.bind('my-event', function(data) {
+      console.log('hello');
+      alert('Received: ' + JSON.stringify(data));
+    });
+  </script>
+</head>
+<body>
+  <h1>Listening with Pusher</h1>
+</body>
+</html>
