ea40525325a938cba2009b2f1c93f9b3a1927e96
lucky8543 <sndp.php@gmail.com>
2025-06-27 11:53:03 +0530

    changes done

--- app/Events/MessageSent.php
+++ app/Events/MessageSent.php
@@ -2,9 +2,9 @@
 
 namespace App\Events;
 
-use Illuminate\Broadcasting\PrivateChannel;
+// use Illuminate\Broadcasting\PrivateChannel;
 use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
-use Illuminate\Queue\SerializesModels;
+// use Illuminate\Queue\SerializesModels;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Broadcasting\Channel;
 
